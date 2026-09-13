2051c41f8d497707cb6e733b24972c3632fe9ee8
Nils-Martin <nilsmartin.robeling@gmail.com>
2026-09-13 04:07:31 +0200

    some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,20 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // Replace with your actual GitHub Pages URL
-  site: 'https://yy-gx.github.io',
-  base: '/Lumina',
-  integrations: [mdx(), sitemap()],
-  output: 'static',
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  markdown: {
-    shikiConfig: {
-      themes: { light: 'github-light', dark: 'github-dark' },
-    },
-  },
+  site: 'https://robeling.space',
 });
+// export default defineConfig({
+//   // Replace with your actual GitHub Pages URL
+//   site: 'https://yy-gx.github.io',
+//   base: '/Lumina',
+//   integrations: [mdx(), sitemap()],
+//   output: 'static',
+//   vite: {
+//     plugins: [tailwindcss()],
+//   },
+//   markdown: {
+//     shikiConfig: {
+//       themes: { light: 'github-light', dark: 'github-dark' },
+//     },
+//   },
+// });
